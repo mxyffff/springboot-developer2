5be9c7ca95235e5eedc8b88b8a177b4e18634b3d
Dakyeom Lee <rua0254@naver.com>
2025-02-28 00:36:42 +0900

    test code add

--- src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDeveloperApplicaiton {
+public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDeveloperApplicaiton.class, args);
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
